ff0dd71c1b1f6db0165da9d9bf3626ec26e1fd4a
kyle <kyle@theablefew.com>
2013-01-22 10:23:31 -0600

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    reload: {
+      port: 4000,
+      liveReload: {},
+      proxy: {
+        host: 'localhost',
+        port: 8000
+      }
+    },
+    trigger: {
+      watchFile: 'index.html'
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          base: 'public'
+        }
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          './public/stylesheets/default.css': './app/assets/stylesheets/default.scss'
+        }
+      },
+      dev: {
+        options: {
+          style: 'expanded',
+          lineNumbers: true
+        },
+        files: {
+          './public/stylesheets/default.css': './app/assets/stylesheets/default.scss'
+        }
+      }
+    },
+    coffee: {
+      compile: {
+        files: {
+          './public/javascripts/default.js': ['./app/assets/javascripts/*.coffee']
+        }
+      }
+    },
+    watch: {
+      files: ['./public/*.html', './app/assets/stylesheets/default.scss', './app/assets/stylesheets/default.scss'],
+      tasks: ['sass', 'coffee', 'reload']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-reload');
+  grunt.registerTask('run', ['connect', 'coffee', 'sass', 'reload', 'watch']);
+  // grunt.registerTask('precomp', ['coffee','sass']);
+
+};
---
+++ README.md
@@ -0,0 +1,12 @@
+# The Able Few Tility
+
+## Install
+
+ * [Node.js](http://nodejs.org/)
+ * `gem install bundler`
+ * `bundle install`
+ * `npm install`
+ * `cd app/assets/stylesheets`
+ * `bourbon install`
+ * `neat install`
+ * `grunt run` or `npm start`
