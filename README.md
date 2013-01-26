f6830e6629cb0fa41302339eea61f5cc3617bb45
kyle <kyle@theablefew.com>
2013-01-26 13:05:18 -0600

    adding in more build commands for rails env

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 ## Install
 
+Edit `.rvmrc` to include your project name : `Ex: rvm 1.9.3@app-name --create`
 Edit `package.json` file to include your application name and version number.    
 `npm install` - If you don't have Node.js installed, you will need to install that before NPM exists   
 `npm start`
--- Tility.js
+++ Tility.js
@@ -7,7 +7,7 @@ var projRoot = pwd();
 var env = process.env.npm_package_config_env;
 
 // load proper Gemfile
-cp('lib/Gemfile.'+env, 'Gemfile');
+cp('lib/tility/Gemfile.'+env, 'Gemfile');
 
 // verify RubyGems is installed
 if (!which('gem')) {
@@ -30,30 +30,34 @@ if (exec('bundle install').code !== 0) {
 // verify environment
 if(env === 'scratch') {
 
-  // change directory for Bourbon and Neat installs
-  cd('app/assets/stylesheets');
+  // build default view
+  cp('lib/tility/index.'+env+'.html', 'index.html');
 
-  if (exec('bourbon install').code !== 0) {
-    echo('Error: Bourbon install failed.');
-    exit(1);
-  }
-
-  if (exec('neat install').code !== 0) {
-    echo('Error: Neat install failed.');
-    exit(1);
-  }
-
-  // If you get this far, you're just about golden!
-  echo(("---------------------------------------").green);
-  echo(("To run your server, just run: grunt run").green);
-  echo(("---------------------------------------").green);
 } else if(env === 'rails') {
+
+  // build default view
+  cp('lib/tility/index.'+env+'.erb', 'app/views/layouts/application.html.erb');
 
   // init rails in current directory
   if (exec('rails new . --skip-bundle -q -s -d mysql').code !== 0) {
     echo('Error: Rails init failed.');
     exit(1);
   }
+
+  // remove tree from sprockets and only include default css
+  sed('-i', ' *= require_tree .', ' *= require "default"', 'app/assets/stylesheets/application.css');
+
+  // add .css extension to default.scss for sprockets
+  mv('app/assets/stylesheets/default.scss', 'app/assets/stylesheets/default.css.scss');
+
+  // no longer need these in rails
+  rm('-rf', '/public/javascripts/');
+  rm('-rf', '/public/stylesheets/');
+  rm('-rf', '/public/images/');
+  rm('/app/assets/javascripts/default.js');
+
+  // moving selectivzr to be a vendor library
+  mv('public/javascripts/selectizr.min.js', 'vendor/assets/javascripts/selectizr.min.js');
 
 } else if(env === 'node') {
   echo(("---------------------------------------").red);
@@ -67,8 +71,23 @@ if(env === 'scratch') {
   exit(1);
 }
 
-// transfer back to project root
-cd(projRoot);
-
 // bring in correct Gruntfile.js for env
-cp('lib/Gruntfile.'+env+'.js', 'Gruntfile.js');
+cp('lib/tility/Gruntfile.'+env+'.js', 'Gruntfile.js');
+
+// change directory for Bourbon and Neat installs
+cd('app/assets/stylesheets');
+
+if (exec('bourbon install').code !== 0) {
+  echo('Error: Bourbon install failed.');
+  exit(1);
+}
+
+if (exec('neat install').code !== 0) {
+  echo('Error: Neat install failed.');
+  exit(1);
+}
+
+// If you get this far, you're just about golden!
+echo(("---------------------------------------").green);
+echo(("To run your server, just run: grunt run").green);
+echo(("---------------------------------------").green);
